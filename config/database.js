edf6d8741fb69bef8ec2ea0d43b732378a92275d
Andy Alexander <andrew.j.alexander@gmail.com>
2021-11-23 12:01:30 -0500

    configuration updates

--- config/database.js
+++ config/database.js
@@ -1,18 +1,17 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 8889),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'root'),
+        client: "mysql2",
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 8889),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME", "root"),
+        password: env("DATABASE_PASSWORD", "root"),
       },
       options: {},
     },
   },
 });
-
--- config/plugins.js
+++ config/plugins.js
@@ -1,22 +1,19 @@
-//Captain's School
-
-/*module.exports = ({ env }) => ({
+module.exports = ({ env }) => ({
   email: {
     provider: "smtp",
     providerOptions: {
-      host: "",
-      port: 587,
-      secure: false,
-      username: "",
-      password: "",
-      rejectUnauthorized: false,
-      tls: {
-        rejectUnauthorized: false,
-      },
+      host: "smtp.gmail.com", //SMTP Host
+      port: 465, //SMTP Port
+      secure: true,
+      username: env("EMAIL_USERNAME", ""),
+      password: env("EMAIL_PASSWORD", ""),
+      rejectUnauthorized: true,
+      requireTLS: true,
+      connectionTimeout: 1,
     },
     settings: {
-      defaultFrom: "",
-      defaultReplyTo: "",
+      from: env("EMAIL_USERNAME", ""),
+      replyTo: env("EMAIL_USERNAME", ""),
     },
   },
-});*/
+});
