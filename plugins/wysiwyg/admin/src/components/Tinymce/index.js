d2ccf6bd6ac92cb80b0826a40f0709dbb0c33b12
Andy Alexander <andrew.j.alexander@gmail.com>
2021-12-09 14:34:59 -0500

    Update to tinymce

--- plugins/wysiwyg/admin/src/components/Tinymce/index.js
+++ plugins/wysiwyg/admin/src/components/Tinymce/index.js
@@ -10,8 +10,22 @@ const TinyEditor = ({ onChange, name, value }) => {
       onEditorChange={(editorContent) => {
         onChange({ target: { name, value: editorContent } });
       }}
-      outputFormat="text"
-      init={{}}
+      outputFormat="html"
+      init={{
+        height: 500,
+        plugins: [
+          "advlist autolink lists link image charmap print preview anchor",
+          "searchreplace visualblocks code fullscreen",
+          "insertdatetime media table paste code help wordcount",
+        ],
+        toolbar:
+          "undo redo | formatselect | " +
+          "bold italic backcolor | alignleft aligncenter " +
+          "alignright alignjustify | bullist numlist outdent indent | " +
+          "removeformat | help",
+        content_style:
+          "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+      }}
     />
   );
 };
